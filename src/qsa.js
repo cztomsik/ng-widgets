c56867a800304f6e207bf1d2bcca249fd776ca8f
Kamil Tomsik <info@tomsik.cz>
2014-02-20 15:14:41 +0100

    phantomjs fix

--- src/qsa.js
+++ src/qsa.js
@@ -6,7 +6,7 @@ module.exports = function(selector){
   var res = [];
 
   [].forEach.call(this, function(el){
-    res.push.apply(res, el.querySelectorAll(selector));
+    res.push.apply(res, [].slice.call(el.querySelectorAll(selector)));
   });
 
   return angular.element(res);
